a6d4e33486fa44971c4a590bbfabcf47c5ee1fd7
Stacey Chen <schen50@stuy.edu>
2025-01-06 23:26:49 -0500

    started adding documentation

---
+++ library.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "library.h"
+
+// adds the song specified to the queue
+void add_song(char* title){
+
+}
+
+// plays the playlist by using execvp and the mpg123 library(?)
+void play(){
+
+}
+
+// stores song onto a file so user has a 'music history' file
+void store_song(char* title){
+
+}
--- library.h
+++ library.h
@@ -1,5 +1,7 @@
 #ifndef library_h
 #define library_h
 //insert function headers here
-
+void add_song(char* title);
+void play();
+void store_song(char* title);
 #endif
