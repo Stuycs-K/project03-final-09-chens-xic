ea37c4d8e34cf2e658929d1c312a5a07c59bccd6
Cathleen Xi <cxi50@stuy.edu>
2025-01-08 13:18:58 -0600

    added in previous code

--- library.h
+++ library.h
@@ -1,7 +1,21 @@
-#ifndef library_h
-#define library_h
-//insert function headers here
-void add_song(char* title);
-void play();
-void store_song(char* title);
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include "node.h"
+
+struct song_node **init();
+void add(struct song_node **library, char *artist, char *title);
+struct song_node *search_song(struct song_node **library, char *artist, char *title);
+struct song_node *search_artist(struct song_node **library, char *artist);
+void print_letter(struct song_node **library, char letter);
+void print_artist(struct song_node ** library, char* artist);
+void print_library(struct song_node ** library);
+void shuffle (struct song_node ** library, int n);
+int delete_song(struct song_node ** library, char* artist, char* title );
+void reset(struct song_node ** library);
+
+
+
+//HELPER METHODS
+int *filledSlots(struct song_node ** library);
 #endif
