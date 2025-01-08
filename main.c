9649f556f44bf248c1fd656af76667162fa1aa77
STACEY_CHEN <schen50@stuy.edu>
2025-01-08 14:50:14 -0500

    started testing play

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "library.h"
+//#include "library.h"
+#include "player.h"
 
 int main(){
-
+  char* song = "Rome.mp3";
+  play(song);
   return 0;
 }
