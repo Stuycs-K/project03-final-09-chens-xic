f3f77e697b7697da331c3207af7c1baf3ed9dd0a
STACEY_CHEN <schen50@stuy.edu>
2025-01-09 14:27:48 -0500

    updated play() so messages from mpg123 quieted

--- player.c
+++ player.c
@@ -10,11 +10,12 @@ void add_song(char* title){
 
 // plays the playlist by using execvp and the mpg123 library(?)
 void play(char *filename){
-  char* args[3];
+  char* args[4];
   char* program = "mpg123";
   args[0] = program;
-  args[1] = filename;
-  args[2] = NULL;
+  args[1] = "-q";
+  args[2] = filename;
+  args[3] = NULL;
   execvp(args[0], args);
 }
 
