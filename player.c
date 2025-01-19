b596474d63523df5e2f6f6e0419384f7ede6792d
Cathleen Xi <cxi50@stuy.edu>
2025-01-18 19:16:44 -0500

    worked on debugging play history

--- player.c
+++ player.c
@@ -12,11 +12,11 @@
 #include "library.h"
 #define BSIZE 256
 
-
 // plays a single song by using execvp and the mpg123 library(?)
-void play(char *filename){
-  char* args[4];
-  char* program = "mpg123";
+void play(char *filename)
+{
+  char *args[4];
+  char *program = "mpg123";
   args[0] = program;
   args[1] = "-q";
   args[2] = filename;
@@ -25,51 +25,64 @@ void play(char *filename){
 }
 
 // plays the playlist built from user input
-void play_list(struct song_node *list){
-  while (list != NULL) {
-      pid_t p = fork();
-      if (p<0){
-        perror("Fork failed.\n");
+void play_list(struct song_node *list)
+{
+  while (list != NULL)
+  {
+    pid_t p = fork();
+    if (p < 0)
+    {
+      perror("Fork failed.\n");
+      exit(1);
+    }
+    else if (p == 0)
+    {
+      char filename[BSIZE];
+      sprintf(filename, "%s_by_%s.mp3", list->title, list->artist);
+      printf("Now playing song %s...\n", filename);
+      play(filename);
+    }
+    else
+    {
+      int status;
+      pid_t child = wait(&status);
+      if (child == -1)
+      {
+        perror("Wait failed.\n");
         exit(1);
       }
-      else if (p==0){
-        char filename[BSIZE];
-        sprintf(filename, "%s_by_%s.mp3", list->title, list->artist);
-        printf("Now playing song %s...\n", filename);
-        play(filename);
+      if (WIFEXITED(status))
+      {
+        list = list->next;
       }
-      else{
-        int status;
-        pid_t child = wait(&status);
-        if (child == -1) {
-          perror("Wait failed.\n");
-          exit(1);
-        }
-        if (WIFEXITED(status)){
-          list = list->next;
-        }
-      }
+    }
   }
 }
 
 // keeps asking user to put one of the commands: play add remove(?) save(?) and calls following function
-struct song_node * prompt_input(struct song_node *list){
+struct song_node *prompt_input(struct song_node *list)
+{
   char buffer[BSIZE];
   printf("Input a command (play   add    show    history): ");
   fgets(buffer, BSIZE, stdin);
-  if (!strcmp(buffer, "add\n")){
+  if (!strcmp(buffer, "add\n"))
+  {
     return get_input(list);
   }
-  else if (!strcmp(buffer, "play\n")){
+  else if (!strcmp(buffer, "play\n"))
+  {
     play_list(list);
   }
-  else if (!strcmp(buffer, "show\n")){
+  else if (!strcmp(buffer, "show\n"))
+  {
     print_playlist(list);
   }
-  else if(!strcmp(buffer, "history\n")){
+  else if (!strcmp(buffer, "history\n"))
+  {
     play_history(list);
   }
-  else{
+  else
+  {
     printf("Please input a valid command.\n");
   }
   return list;
@@ -78,7 +91,8 @@ struct song_node * prompt_input(struct song_node *list){
 // takes a song_node pointer as parameter (list), reads user input and makes it into a song node, which is added to list
 // user input should be in this format: song_name song_artist, song names and song artists should not have spaces
 // input added to library or to list?
-struct song_node * get_input(struct song_node * list){
+struct song_node *get_input(struct song_node *list)
+{
   char buffer[BSIZE];
   char artist[BSIZE];
   char title[BSIZE];
@@ -88,7 +102,7 @@ struct song_node * get_input(struct song_node * list){
   sscanf(buffer, "%s %s", title, artist);
 
   store_song(title, artist);
-  return insert_front(list,artist,title);
+  return insert_front(list, artist, title);
 }
 
 // stores song onto a file so user has a 'music history' file
@@ -96,14 +110,15 @@ struct song_node * get_input(struct song_node * list){
 // creates/opens backup file
 // gets stat information of mp3
 // append song name and stat info into one string
-//write to file
-void store_song(char *title, char *artist){
+// write to file
+void store_song(char *title, char *artist)
+{
   int fd = open("Backup", O_WRONLY | O_APPEND | O_CREAT, 0666);
-  if(fd < 0) printf("%s\n", strerror(errno));
+  if (fd < 0)
+    printf("%s\n", strerror(errno));
 
-
-  //songPath = catPath("/MUSICFOLDER/", song_title); for when we move music into folder
-  //struct dirent *entry;
+  // songPath = catPath("/MUSICFOLDER/", song_title); for when we move music into folder
+  // struct dirent *entry;
   /*
   char artist[BSIZE];
   strcpy(song->artist, artist);
@@ -113,51 +128,67 @@ void store_song(char *title, char *artist){
   char filename[BSIZE];
   sprintf(filename, "%s_by_%s.mp3", title, artist);
 
-  struct stat * stat_buffer;
+  struct stat *stat_buffer;
   stat_buffer = malloc(sizeof(struct stat));
   stat(filename, stat_buffer);
-//  printf("song size: %ld\n", stat_buffer->st_size);
+  //  printf("song size: %ld\n", stat_buffer->st_size);
   char buff[BSIZE];
-  sprintf(buff, "%s |%s |%ld bytes\n", title, artist, stat_buffer->st_size);
+  sprintf(buff, "%ld %s %s\n", stat_buffer->st_size, title, artist);
 
-//  printf("%s\n", buff);
-  int bytesWritten = write(fd, buff, strlen(buff)+1);
-//  printf("bytes written: %d sizeof buff: %ld\n", bytesWritten, sizeof(buff));
-
+  //  printf("%s\n", buff);
+  int bytesWritten = write(fd, buff, strlen(buff) + 1);
+  //  printf("bytes written: %d sizeof buff: %ld\n", bytesWritten, sizeof(buff));
 }
 
-
-char* catPath(char *PATH, char *entryName){
-    int size = strlen(PATH) + strlen(entryName);
-    char str[size];
-    char *buffer = str;
-    strcpy(buffer, PATH);
-    strncat(buffer, "/", strlen(buffer)+2);
-    strncat(buffer, entryName, size+2);
-  //printf("%s\n", buffer);
-    return buffer;
+char *catPath(char *PATH, char *entryName)
+{
+  int size = strlen(PATH) + strlen(entryName);
+  char str[size];
+  char *buffer = str;
+  strcpy(buffer, PATH);
+  strncat(buffer, "/", strlen(buffer) + 2);
+  strncat(buffer, entryName, size + 2);
+  // printf("%s\n", buffer);
+  return buffer;
 }
 
-//plays the user old list from backup file
-// malloc new song_node list
-// read from Backup, parse string into nodes and add to list
-//call play_list with new list.
+// plays the user old list from backup file
+//  malloc new song_node list
+//  read from Backup, parse string into nodes and add to list
+// call play_list with new list.
 
-void play_history(struct song_node * list){
+void play_history(struct song_node *list)
+{
   FILE *backup = fopen("Backup", "r");
   char *strbuff;
   char buff[100];
   strbuff = buff;
-
-  while(fgets(buff, 100, backup) != NULL){
-    printf("inside while\n");
-    char *artist; char *title;
-    printf("about to strsep\n");
+  while (fgets(buff, 100, backup))
+  {
+    //  printf("inside while\n");
+    strbuff[strlen(strbuff) - 1] = '\0';
+    long int bytes;
+    char title[100];
+    char artist[100];
+    // printf("about to sscanf\n");
+    sscanf(buff, "%ld %s %s", &bytes, title, artist);
+    /*
+    char *artist;
+    char *title;
+    // printf("about to strsep\n");
     title = strsep(&strbuff, "|");
-    artist = strsep(&strbuff, " ");
-    printf("parsed out title: %s, artist: %s\n", title, artist);
+    if (title == NULL)
+    {
+      break;
+    }
+    artist = strbuff;
+    */
+    printf("%s %s\n", title, artist);
+    // printf("%s\n", buff);
 
-    printf("adding to playlist\n");
+    // printf("adding to playlist\n");
     list = insert_front(list, artist, title);
+    // printf("after added to playlist\n");
   }
+  play_list(list);
 }
