4468c0ce9101f33a18de3b1f9b74bbda8e229706
Cathleen Xi <cxi50@stuy.edu>
2025-01-19 16:28:35 -0500

    added sighandler

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include "library.h"
 #include "player.h"
 #include "node.h"
 
+void sighandler(int signo)
+{
+  if (signo == SIGINT)
+  {
+    printf("\nClosing music player...\n");
+    exit(0);
+  }
+}
+
 int main()
 {
+  signal(SIGINT, sighandler);
   struct song_node *list = NULL;
   while (1)
   {
